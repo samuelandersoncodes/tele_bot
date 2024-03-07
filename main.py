b813dbc988e0c3faebe48cda78718e3a3ad9e654
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-03-07 23:12:47 +0100

    feat: Add basic message handling functionality

--- main.py
+++ main.py
@@ -3,4 +3,10 @@ from env import TELEGRAM_BOT_TOKEN
 
 bot = telebot(TELEGRAM_BOT_TOKEN, parse_mode=None)
 
+
+@bot.message_handler(commands=["help", "hello"])
+def send_help(message):
+    bot.reply_to(message, "Hello, auto bot here!")
+
+
 bot.polling()
