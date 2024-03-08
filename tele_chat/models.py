71b4c1d6a85f8a9be933f82134075c17e43bc43e
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-03-08 13:42:04 +0100

    feat: Add telegram message model

--- tele_chat/models.py
+++ tele_chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class TelegramMessage(models.Model):
+    #  This class stores telegram messages
+    chat_id = models.CharField(max_length=255)
+    message_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.chat_id} - {self.message_text}"
