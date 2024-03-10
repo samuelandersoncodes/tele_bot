6dc339772cac71a94efe1f860ab8483a72e6909a
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-03-10 18:10:39 +0100

    refactor: Modify telechat view to poll request

--- tele_chat/views.py
+++ tele_chat/views.py
@@ -6,18 +6,37 @@ import json
 from .models import TelegramMessage
 import os
 
-telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
-
 
 @csrf_exempt
 @require_POST
-def telegram_webhook(request):
-    # This function handles incoming messages
-    data = json.loads(request.body.decode("utf-8"))
-    # Extract relevant information from the Telegram update
-    chat_id = data["message"]["chat"]["id"]
-    message_text = data["message"]["text"]
-    # Save message to the database
-    TelegramMessage.objects.create(chat_id=chat_id, message_text=message_text)
-    # Respond to the Telegram server to acknowledge message receipt
+def telegram_polling(request):
+    """
+    This function retrieves updates from
+    the Telegram server, processes it and
+    saves the received messages and chat id
+    """
+    # Telegram bot token retrieved from env
+    telegram_bot_token = os.environ.get("TELEGRAM_BOT_TOKEN")
+    # Get the latest update id from the database
+    latest_update_id = (
+        TelegramMessage.objects.latest("id").id
+        if TelegramMessage.objects.exists()
+        else None
+    )
+    # Prepare the request payload
+    payload = {
+        "offset": latest_update_id + 1 if latest_update_id else None,
+        "timeout": 30,
+    }
+    # Make a request to the Telegram server to get updates
+    response = requests.get(
+        f"https://api.telegram.org/bot{telegram_bot_token}/getUpdates", params=payload
+    )
+    updates = response.json()["result"]
+    # Process and save the received messages
+    for update in updates:
+        chat_id = update["message"]["chat"]["id"]
+        message_text = update["message"]["text"]
+        TelegramMessage.objects.create(chat_id=chat_id, message_text=message_text)
+    # Respond to the Telegram server to acknowledge updates reciept
     return JsonResponse({"status": "ok"})
