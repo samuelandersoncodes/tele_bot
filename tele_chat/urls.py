609df04adfa5838e2fb236948c8cfde9522d418d
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-03-10 18:14:56 +0100

    refactor: Modify telechat url path to suit view

--- tele_chat/urls.py
+++ tele_chat/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import telegram_webhook
+from .views import telegram_polling
 
 urlpatterns = [
-    path("webhook/", telegram_webhook, name="telegram_webhook"),
+    path('poll/', telegram_polling, name='telegram_polling'),
 ]
