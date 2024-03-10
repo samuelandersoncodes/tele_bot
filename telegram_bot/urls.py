62ca98f8d9c022058a2ff2c391b8ac74c33bc517
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-03-10 17:17:04 +0100

    fix: Refactor telechat url path to fix module error

--- telegram_bot/urls.py
+++ telegram_bot/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("telegramchat/", include("telegramchat.urls")),
+    path("tele_chat/", include("tele_chat.urls")),
 ]
