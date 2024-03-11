df900264982af47646a5eca9d86dd49d4eea1a39
samuelandersoncodes <112104150+samuelandersoncodes@users.noreply.github.com>
2024-03-11 12:26:49 +0100

    chore: Add test telegram message url path

--- tele_chat/urls.py
+++ tele_chat/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import telegram_polling
+from .views import telegram_polling, test_telegram_message
 
 urlpatterns = [
-    path('poll/', telegram_polling, name='telegram_polling'),
+    path("poll/", telegram_polling, name="telegram_polling"),
+    path("test_telegram_message/", test_telegram_message, name="test_telegram_message"),
 ]
